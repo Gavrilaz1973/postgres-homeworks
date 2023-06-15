a33814b37e906d1f1f762eaf13dc3ecf8c595799
Dmitry Gavrilenko <gavrilaz0076@gmail.com>
2023-06-15 16:27:56 +0300

    homework-1 for checking

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,26 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+rows = []
+with open('/home/gavrilaz/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv', 'r',
+          encoding='windows-1251') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        rows.append([row['order_id'], row['customer_id'], row['employee_id'], row['order_date'], row['ship_city']])
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="Podoseniha2012!")
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            for row in rows:
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
+            cur.execute("SELECT * FROM orders;")
+            rows_all = cur.fetchall()
+            for row in rows_all:
+                print(row)
+finally:
+    conn.close()
+
+
